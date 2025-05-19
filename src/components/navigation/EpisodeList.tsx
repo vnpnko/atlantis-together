149c1b9aa028c3fe868d67a82b5f19560af832d4
Ivan Pylypenko <vn.pnko@gmail.com>
2025-05-19 18:49:39 +0200

    Add EpisodeList and ListHeader components to reuse them for navigation component fow both mobile and desktop screens

---
+++ src/components/navigation/EpisodeList.tsx
@@ -0,0 +1,43 @@
+import { VStack } from "@chakra-ui/react";
+import CustomButton from "../ui/CustomButton.tsx";
+import locationsData from "../../assets/data/locations.json";
+import { GlobeLocation } from "../../models/globe/GlobeLocation.ts";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
+interface EpisodeListProps {
+  current: string | null;
+  hovered: string | null;
+  setHovered: (e: string | null) => void;
+}
+
+export const EpisodeList: React.FC<EpisodeListProps> = ({
+  current,
+  hovered,
+  setHovered,
+}) => {
+  const episodes = locationsData as GlobeLocation[];
+
+  const navigate = useNavigate();
+
+  const handleSceneChange = (sceneId: string) => {
+    navigate(`/?scene=${sceneId}`);
+  };
+
+  return (
+    <VStack spacing={3} px={2} overflowY="auto">
+      {episodes.map(({ label }) => (
+        <CustomButton
+          key={label}
+          label={label}
+          isActive={current === label}
+          isHovered={hovered === label}
+          onClick={() => handleSceneChange(label)}
+          onMouseEnter={() => setHovered(label)}
+          onMouseLeave={() => setHovered(null)}
+          justifyContent="flex-start"
+        />
+      ))}
+    </VStack>
+  );
+};
